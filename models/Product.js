b2981e34cee7c35e03b3405195dc6a3bccb88407
Yves-Henry Moraille <yves-henrymoraille@Yves-Henrys-Air.lan>
2023-01-17 14:32:22 -0500

    created heroku

--- models/Product.js
+++ models/Product.js
@@ -9,8 +9,23 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
   },
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+},
+price: {
+  type: DataTypes.DECIMAL(10,2),
+  allowNull: false,
+  validate: {
+      isDecimal: true,
+    // define columns
+}}},
   {
     sequelize,
     timestamps: false,
